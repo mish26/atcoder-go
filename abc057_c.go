4b5cd5ec4a42c4533093fa5b76205e20c787d4a9
mishrun <mishrun.git@gmail.com>
2020-09-03 23:22:27 +0900

    add abc057_c.go

---
+++ abc057_c.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+
+    // F(A,B) を「10 進表記における、A の桁数と B の桁数のうち大きい方」
+    // 2 つの正の整数の組 (A,B) が N=A×B を満たすように動くときの F(A,B) の最小値を出力せよ。
+    // √nの約数を全探査
+
+}
